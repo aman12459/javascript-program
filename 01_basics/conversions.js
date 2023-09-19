730ad6112953285c05f727a1528ce4712afa42ae
Aman kumar <114151098+aman12459@users.noreply.github.com>
2023-09-19 08:56:30 +0000

    data type summry

--- 01_basics/conversions.js
+++ 01_basics/conversions.js
@@ -10,16 +10,80 @@
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = 1
+//let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false
 // "" => false
 //"Aman" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+/*let value = 3 
+let negValue = -value
+let str1 ="hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+
+console.log(str3)
+console.log("1" +2);
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+*/
+
+//console.log(true)
+
+
+/*let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
+console.log(2 == 1)
+console.log(2 >=1)
+console.log( 2 < 1)
+*/
+
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+//console.log(null > 0)
+//console.log(null == 0)
+//console.log(null >= 0);
+
+
+//console.log(undefined > 0)
+//console.log(undefined == 0)
+//console.log(undefined >= 0);
+
+//console.log("2"=== 2);
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false;
+const outsideTemp = null
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+const bigNumber = 23445454565546654n
+
+// Non primitive
+// Array, Objects, Function
+
+const heros = ["Aman", "vishal"];
+let  myObj ={
+    name: "Aman",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello Wold")
+    console.log(typeof heros)
+}
