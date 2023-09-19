44224963d52c53d9f218f7a85ee83b8025b0b167
Aman kumar <114151098+aman12459@users.noreply.github.com>
2023-09-19 06:29:01 +0000

    data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 13425
+let accountEmail = "thakur2002aman@gmail.com"
+let accountPassword = "12345"
+let accountCity = "Dharmshla"
+let accountState;
+
+
+accountCity = "Himachal"
+accountPassword = "54938404"
+accountEmail = "aman@gmail.com"
+
+console.log(accountId)
+
+console.table([accountCity, accountEmail, accountId, accountPassword, accountState])
---
+++ 01_basics/dataType.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let name = "Aman"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bight
+//string => ""
+//boolean => true/ false
+//null => standalone value
+//underfined => abhi value define nhi ki
+//symbol => unique 
+
+// object
+
+console.log(typeof null)
